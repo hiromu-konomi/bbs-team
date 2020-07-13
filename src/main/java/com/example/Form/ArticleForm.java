3d2d1c682eb3835f306276c324c09f481ccb8028
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2020-07-13 10:49:42 +0900

    Create new Article form

---
+++ src/main/java/com/example/Form/ArticleForm.java
@@ -0,0 +1,80 @@
+package com.example.Form;
+
+public class ArticleForm {
+
+    private String id;
+    private String name;
+    private String content;
+
+    public ArticleForm() {
+    }
+
+    public ArticleForm(String id, String name, String content) {
+        this.id = id;
+        this.name = name;
+        this.content = content;
+    }
+
+
+    /**
+     * @return String
+     */
+    public String getId() {
+        return this.id;
+    }
+
+
+    /**
+     * @param id
+     */
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    /**
+     * @return String
+     */
+    public String getName() {
+        return this.name;
+    }
+
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    /**
+     * @return String
+     */
+    public String getContent() {
+        return this.content;
+    }
+
+
+    /**
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    /**
+     * @return String
+     */
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", name='" + getName() + "'" +
+            ", content='" + getContent() + "'" +
+            "}";
+    }
+
+
+}
